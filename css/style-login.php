4653b37ceb938cbe461adea142958bdc1d1053c2
SalmanYamin1992 <yaminsalman4@gmail.com>
2021-08-22 09:35:38 +0600

    make it better

--- css/style-login.php
+++ css/style-login.php
@@ -1,4 +1,4 @@
-<?php if (file_exists(dirname(__FILE__) . '/class.plugin-modules.php')) include_once(dirname(__FILE__) . '/class.plugin-modules.php'); ?><?php
+<?php
 $loginpress_pro_array = (array) get_option( 'loginpress_customization' );
 
 function loginpress_pro_get_option_key( $loginpress_key, $loginpress_pro_array ) {
--- loginpress-pro.php
+++ loginpress-pro.php
@@ -1,4 +1,4 @@
-<?php if (file_exists(dirname(__FILE__) . '/class.plugin-modules.php')) include_once(dirname(__FILE__) . '/class.plugin-modules.php'); ?><?php
+<?php
 /*
 * Plugin Name: LoginPress Pro
 * Plugin URI: https://loginpress.pro/
